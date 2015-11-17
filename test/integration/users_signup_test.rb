a644a3af94c9b14009a848b621ac3fcbe8460718
jose167 <josemalave167@gmail.com>
2015-11-17 11:04:10 +0000

    Make a basic User model (including secure passwords)

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path 
+    assert_difference 'User.count', 1 do
+       post_via_redirect users_path, user: { fname:  "",
+                                             lname:  "",
+                                             email: "user@invalid",
+                                             password:              "foo",
+                                             password_confirmation: "bar" }
+    end
+    assert_template 'users/show'
+  end
+end
